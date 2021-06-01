240672fe14049b65ce4876ab6a68c063351ac3c9
noahwastaken <tnoah12@gmail.com>
2021-06-01 11:59:58 -0400

    Added functions modules from source

--- Trigger.cpp
+++ Trigger.cpp
@@ -20,5 +20,87 @@ bool GetProcess(const wchar_t* szName)
 
 		CloseHandle(hProcess);
 	}
-
 	return false;
+
+	DWORD GetModule(const wchar_t* szModule)
+	{
+		if (auto hModule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, G::Proc::m_dwPID))
+		{
+			MODULEENTRY32 ModEntry;
+			ModEntry.dwSize = sizeof(ModEntry);
+
+			while (Module32Next(hModule, &ModEntry))
+			{
+				if (!wcscmp(ModEntry.szModule, szModule))
+				{
+					CloseHandle(hModule);
+					return reinterpret_cast<DWORD>(ModEntry.modBaseAddr);
+				}
+			}
+
+			CloseHandle(hModule);
+		}
+
+		return 0x0;
+	}
+
+	int main()
+	{
+		SetConsoleTitleW(L"Trigger Example");
+
+		if (GetProcess(L"insert.exe"))
+		{
+			if (const auto dwClient = GetModule(L"insert.dll"))
+			{
+				wprintf(L" If Running!\n");
+
+				while (!(GetAsyncKeyState(VK_F11) & 0x01))
+				{
+					if (const auto& dwLocal = G::Mem::Read<DWORD>(dwClient + G::Off::dwLocalPlayer))
+					{
+						const auto nCrossID = G::Mem::Read<int>(dwLocal + G::Off::m_iCrosshairId);
+
+						if (nCrossID <= 64 && nCrossID >= 0)
+						{
+							if (const auto& dwEntity = G::Mem::Read<DWORD>(dwClient + G::Off::dwEntityList + (nCrossID * 0x10)))
+							{
+								if (G::Mem::Read<byte>(dwEntity + G::Off::m_lifeState) == LIFE_ALIVE)
+								{
+									if (G::Mem::Read<int>(dwEntity + G::Off::m_iTeamNum) != G::Mem::Read<int>(dwLocal + G::Off::m_iTeamNum))
+									{
+										if (GetAsyncKeyState(VK_XBUTTON2) & 0x8000)
+										{
+
+											mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+
+											std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+											mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+										}
+									}
+								}
+							}
+						}
+					}
+
+					std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				}
+			}
+			else
+			{
+				wprintf(L"Not able to access insert.dll!\n");
+			}
+		}
+		else
+		{
+			wprintf(L"Did not find insert.exe!\n");
+		}
+
+		if (G::Proc::m_hProc)
+			CloseHandle(G::Proc::m_hProc);
+
+		wprintf(L"Quitted!\n");
+		_wsystem(L"pause");
+
+		return EXIT_SUCCESS;
+	}
